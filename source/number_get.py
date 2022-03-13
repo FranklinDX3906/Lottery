f01abea636ca03e75ce96d95be0b492d658a0d7b
张毅恒 <993628121@qq.com>
2022-03-13 21:43:35 +0800

    2022.03.13上传最新的结构以及readme文档

--- source/number_get.py
+++ source/number_get.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 """
-功能：获取最近100期的所有号码
+功能：获取大乐透能够获得的所有代码
 作者：DX3906 993628121@qq.com
 """
 
@@ -31,11 +31,15 @@ def number_get(num=100):
 
     data_1 = []
     for i in range(1, 91):
-        url = 'http://www.lottery.gov.cn/historykj/history_' + str(
-            i) + '.jspx?_ltype=dlt'
+        url = 'https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=1'
         data = requests.get(url).text
-        data = bs(data, 'lxml')
-        data = data.find('tbody').find_all('tr')
+        data = str(bs(data, 'lxml').p.next).split(',')
+        # data = data.body
+        # print(data.a)
+        data.replace('false', 'False')
+        data = eval(data)
+        data = data.find('tbody id="historyData"')
+        data = data['lotteryUnsortDrawresult']
         for content in data:
             number = content.get_text().strip().replace('\r', '').replace(
                 '\t', '').replace('\n', ' ')
